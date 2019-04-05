23bfa0c3d69c853266e1fd25b481d46f4c57c959
David Bazile <dbazile@users.noreply.github.com>
2019-04-05 19:25:00 -0400

    Add basic file index and download

---
+++ storeroom.py
@@ -0,0 +1,27 @@
+import os
+
+import flask
+
+
+PUBLIC_DIR = os.path.dirname(__file__) + '/public'
+
+
+# Create files directory if needed
+if not os.path.exists(PUBLIC_DIR):
+    print(f'Creating directory {PUBLIC_DIR}...')
+    os.mkdir(PUBLIC_DIR)
+
+
+app = flask.Flask(
+    name='storeroom',
+    static_folder=PUBLIC_DIR,
+    static_url_path='/files')
+
+
+@app.route('/')
+def index():
+    files = []
+    for f in os.listdir(PUBLIC_DIR):
+        files.append(f)
+
+    return f'files: {", ".join(files)}\n'
